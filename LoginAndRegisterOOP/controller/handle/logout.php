b4edea96148f261ca9231dab232199d72d791d79
Ahmed Osama <69996508+Ahmedosama000@users.noreply.github.com>
2023-09-19 23:12:36 +0300

    Delete LoginAndRegisterOOP directory

--- LoginAndRegisterOOP/controller/handle/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-session_start();
-unset($_SESSION['user-email']);
-unset($_SESSION['username']);
-header('location:../../view/pages/login.php');
--- LoginAndRegisterOOP/view/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-light mt-5 p-3 text-center text-dark">
-    Copyright &copy; 2023 All Right Reserved
-    <br> Created By <span class="text-secondary opacity-75">
-        Ahmed Osama.
-    </span>
-</footer>
